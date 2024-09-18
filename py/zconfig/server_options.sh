2de753b30ac7a1f922b52b46215ac7002697816e
putintin420 <psnonlineshit@gmail.com>
2024-09-18 13:20:52 +0000

    linux server options

---
+++ py/zconfig/server_options.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# PROGRAM ROUTES
+export nut="ztools/squirrel.py"
+export pycommand="python3"
+export pycommandw="python3"
+
+# SERVER OPTIONS
+export start_minimized="yes"
+export videoplayback="true"
+export port="9001"
+export host="0.0.0.0"
+export noconsole="true"
+export ssl="false"
+
+# Add this line to set the dec_keys path
+export dec_keys="ztools/keys.txt"
